9153ee17d1729791ce2ea08b4acca3f66b56afbc
CharlyCMD <carlos.cimd@outlook.com>
2023-07-11 11:55:41 -0600

    adding socket

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const http = require("http");
+const socketIO = require("socket.io");
+// const port = require("./config");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
 
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*", // Cambia esto a la URL permitida de tu aplicación cliente
+    methods: ["GET", "POST"], // Cambia esto según los métodos que necesites permitir
+    credentials: true, // Si necesitas enviar cookies en las solicitudes, establece esto en true
+  },
+});
+
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -16,6 +29,12 @@ app.get("/", (req, res) => {
   res.json(data);
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("Un cliente se ha conectado");
+  // Enviar mensaje de bienvenida al cliente
+  socket.emit("welcome", "¡Bienvenido al servidor Socket.io!");
+});
+
+server.listen(port, () => {
   console.log(`API escuchando en el puerto ${port}`);
 });
