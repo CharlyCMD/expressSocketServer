602550d88675b2061db3ded0469ca2401a757bbd
CharlyCMD <carlos.cimd@outlook.com>
2023-07-11 12:02:34 -0600

    adding complete socket server

--- index.js
+++ index.js
@@ -29,10 +29,125 @@ app.get("/", (req, res) => {
   res.json(data);
 });
 
+const rooms = new Map(); // Mapa para almacenar los rooms y sus jugadores
+let turnPlayer1 = true;
+let turnPlayer2 = false;
+
+let board = Array(9).fill(null);
+
+const updateBoard = ({ index, turn, winner }) => {
+  if (board[index] || winner) {
+    console.log("retornamos aqui porque deberia de ser null");
+    return false;
+  }
+  console.log("continuamos con la ejecucion");
+  board[index] = turn;
+  return true;
+};
+
+const changeTurns = () => {
+  console.log("changing turns");
+  turnPlayer1 = !turnPlayer1;
+  turnPlayer2 = !turnPlayer2;
+};
+
+const handleUpdateBoard = ({ index, turn, winner, room }) => {
+  const isValidSelection = updateBoard({ index, turn, winner });
+  if (isValidSelection) {
+    changeTurns();
+    room.player1.emit("changingTurns", {
+      turnState: turnPlayer1,
+      board: board,
+    });
+    room.player2.emit("changingTurns", {
+      turnState: turnPlayer2,
+      board: board,
+    });
+  }
+};
+
 io.on("connection", (socket) => {
-  console.log("Un cliente se ha conectado");
-  // Enviar mensaje de bienvenida al cliente
-  socket.emit("welcome", "¡Bienvenido al servidor Socket.io!");
+  socket.emit("welcome", "¡Bienvenido al servidor Socket.io! Lluvia");
+
+  socket.on("joinRoom", (roomName) => {
+    socket.join(roomName);
+    console.log(`Jugador se unió al room: ${roomName}`);
+
+    const currentRoom = io.sockets.adapter.rooms.get(roomName);
+    const numPlayers = currentRoom ? currentRoom.size : 0;
+    if (numPlayers === 1) {
+      // Primer jugador en unirse al room
+      rooms.set(roomName, {
+        player1: socket,
+        player2: null,
+      });
+
+      socket.emit("roomJoined", {
+        success: true,
+        message: `Te has unido al room exitosamente`,
+        turn: "🦖",
+        roomName,
+        activeTurn: turnPlayer1,
+        board,
+      });
+    } else if (numPlayers === 2) {
+      // Segundo jugador en unirse al room
+      const room = rooms.get(roomName);
+      room.player2 = socket;
+
+      rooms.set(roomName, room);
+
+      socket.emit("roomJoined", {
+        success: true,
+        message: `Te has unido al room exitosamente`,
+        turn: "🎸",
+        roomName,
+        activeTurn: turnPlayer2,
+        board,
+      });
+      room.player1.emit(
+        "message",
+        "El segundo jugador ha llegado. Comienza la partida."
+      );
+    } else {
+      // Room lleno
+      socket.emit(
+        "message",
+        "El room está lleno. Por favor, intenta más tarde."
+      );
+      socket.disconnect(true);
+    }
+  });
+
+  socket.on("playerEvent", ({ roomName, turn, index, winner }) => {
+    const room = rooms.get(roomName);
+    console.log("QUE ESTA PASANDO", room, rooms, roomName);
+
+    if (room) {
+      if (room.player1 === socket) {
+        handleUpdateBoard({ index, turn, winner, room });
+      } else if (room.player2 === socket) {
+        handleUpdateBoard({ index, turn, winner, room });
+      }
+    }
+  });
+
+  socket.on("resetGame", (roomName) => {
+    const room = rooms.get(roomName);
+    if (room) {
+      turnPlayer1 = true;
+      turnPlayer2 = false;
+      board = Array(9).fill(null);
+      room.player1.emit("changingTurns", {
+        turnState: turnPlayer1,
+        board: board,
+      });
+      room.player2.emit("changingTurns", {
+        turnState: turnPlayer2,
+        board: board,
+      });
+    }
+  });
 });
 
 server.listen(port, () => {
