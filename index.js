21e10f5cb024440cde0c70b687cc098896112222
CharlyCMD <carlos.cimd@outlook.com>
2023-07-11 11:42:30 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/api/data", (req, res) => {
+  const data = [
+    { id: 1, name: "Objeto 1" },
+    { id: 2, name: "Objeto 2" },
+    { id: 3, name: "Objeto 3" },
+  ];
+
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`API escuchando en el puerto ${port}`);
+});
