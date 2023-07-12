9c6b5e1c088de04400cc6a5768387f3a9110250f
CharlyCMD <carlos.cimd@outlook.com>
2023-07-12 16:24:22 -0600

    nueva logica agregada

--- index.js
+++ index.js
@@ -34,30 +34,36 @@ let turnPlayer2 = false;
 
 let board = Array(9).fill(null);
 
-const updateBoard = ({ index, turn, winner }) => {
+const updateBoard = ({ index, turn, winner, board }) => {
   if (board[index] || winner) {
     return false;
   }
   board[index] = turn;
-  return true;
+  return {
+    updatedBoard: board,
+    isValidSelection: true,
+  };
 };
 
-const changeTurns = () => {
-  turnPlayer1 = !turnPlayer1;
-  turnPlayer2 = !turnPlayer2;
-};
-
-const handleUpdateBoard = ({ index, turn, winner, room }) => {
-  const isValidSelection = updateBoard({ index, turn, winner });
+const newHandleUpdateBoard = ({ roomName, index, turn, winner, room }) => {
+  const { updatedBoard, isValidSelection } = updateBoard({
+    index,
+    turn,
+    winner,
+    board: room.board,
+  });
   if (isValidSelection) {
-    changeTurns();
-    room.player1.emit("changingTurns", {
-      turnState: turnPlayer1,
-      board: board,
+    room.player1.activeTurn = !room.player1.activeTurn;
+    room.player2.activeTurn = !room.player2.activeTurn;
+    room.board = updatedBoard;
+    rooms.set(roomName, room);
+    room.player1.socket.emit("changingTurns", {
+      turnState: room.player1.activeTurn,
+      board: room.board,
     });
-    room.player2.emit("changingTurns", {
-      turnState: turnPlayer2,
-      board: board,
+    room.player2.socket.emit("changingTurns", {
+      turnState: room.player2.activeTurn,
+      board: room.board,
     });
   }
 };
@@ -66,134 +72,114 @@ const rooms = new Map(); // Mapa para almacenar los rooms y sus jugadores
 
 io.on("connection", (socket) => {
   socket.emit("welcome", "¡Bienvenido al servidor Socket.io! Lluvia");
-
   socket.on("joinRoom", (roomName) => {
-    socket.join(roomName);
-    console.log(`Jugador se unió al room: ${roomName}`);
-
     const primerRoom = rooms.get(roomName);
     if (typeof primerRoom === "undefined") {
-      // Primer jugador en unirse al room
+      socket.join(roomName);
       rooms.set(roomName, {
-        player1: socket,
+        player1: {
+          socket,
+          activeTurn: true,
+          turn: "🦖",
+          playerType: "player1",
+        },
         player2: null,
+        board: Array(9).fill(null),
       });
-
+      const room = rooms.get(roomName);
       socket.emit("roomJoined", {
         success: true,
         message: `Te has unido al room exitosamente`,
-        turn: "🦖",
         roomName,
-        activeTurn: turnPlayer1,
-        board,
-        playerType: "player1",
+        turn: room.player1.turn,
+        activeTurn: room.player1.activeTurn,
+        board: room.board,
+        playerType: room.player1.playerType,
       });
-    } else if (typeof primerRoom !== "undefined") {
-      if (primerRoom.player1 === null && primerRoom.player2 === null) {
-        primerRoom.player1 = socket;
-        rooms.set(roomName, primerRoom);
-        socket.emit("roomJoined", {
-          success: true,
-          message: `Te has unido al room exitosamente`,
-          turn: "🦖",
-          roomName,
-          activeTurn: turnPlayer1,
-          board,
-          playerType: "player1",
-        });
-      } else if (primerRoom.player1 !== null && primerRoom.player2 === null) {
-        primerRoom.player2 = socket;
-        rooms.set(roomName, primerRoom);
-        socket.emit("roomJoined", {
-          success: true,
-          message: `Te has unido al room exitosamente`,
-          turn: "🎸",
-          roomName,
-          activeTurn: turnPlayer2,
-          board,
-          playerType: "player2",
-        });
-        primerRoom.player1.emit(
-          "message",
-          "El segundo jugador ha llegado. Comienza la partida."
-        );
-      } else if (primerRoom.player1 === null && primerRoom.player2 !== null) {
-        primerRoom.player1 = socket;
-        rooms.set(roomName, primerRoom);
-        socket.emit("roomJoined", {
-          success: true,
-          message: `Te has unido al room exitosamente`,
-          turn: "🦖",
-          roomName,
-          activeTurn: turnPlayer1,
-          board,
-          playerType: "player1",
-        });
-        primerRoom.player2.emit(
-          "message",
-          "El segundo jugador ha llegado. Comienza la partida."
-        );
-      } else {
-        // Room lleno
-        socket.emit(
-          "message",
-          "El room está lleno. Por favor, intenta más tarde."
-        );
-        socket.disconnect(true);
-      }
+    } else if (
+      typeof primerRoom !== "undefined" &&
+      primerRoom.player2 === null
+    ) {
+      socket.join(roomName);
+      primerRoom.player2 = {
+        socket,
+        activeTurn: false,
+        turn: "🎸",
+        playerType: "player2",
+      };
+      rooms.set(roomName, primerRoom);
+      socket.emit("roomJoined", {
+        success: true,
+        message: `Te has unido al room exitosamente`,
+        roomName,
+        turn: primerRoom.player2.turn,
+        activeTurn: primerRoom.player2.activeTurn,
+        board: primerRoom.board,
+        playerType: primerRoom.player2.playerType,
+      });
+    } else {
+      socket.emit(
+        "message",
+        "El room está lleno. Por favor, intenta más tarde."
+      );
+      // socket.disconnect(true);
     }
   });
 
-  socket.on("leaveRoom", ({ currentRoom, player }) => {
-    const room = rooms.get(currentRoom);
-    room[player] = null;
-    // socket.leave(currentRoom);
-    turnPlayer1 = true;
-    turnPlayer2 = false;
-    board = Array(9).fill(null);
-    // if (room?.player1 === null && room?.player2 === null) {
-    //   console.log("deberiamos de borrar el room", rooms);
-    //   return;
-    // }
-    if (player === "player1") {
-      room?.player2?.emit("changingTurns", {
-        turnState: turnPlayer2,
-        board: board,
-      });
-    } else {
-      room?.player1?.emit("changingTurns", {
-        turnState: turnPlayer1,
-        board: board,
-      });
-    }
+  socket.on("leaveRoom", ({ currentRoom }) => {
+    const primerRoom = rooms.get(currentRoom);
+    primerRoom.player1.socket.emit("clearGame", "limpiando juego");
+    primerRoom.player2.socket.emit("clearGame", "limpiando juego");
+    rooms.delete(currentRoom);
   });
 
   socket.on("playerEvent", ({ roomName, turn, index, winner }) => {
     const room = rooms.get(roomName);
+
     if (room) {
-      if (room.player1 === socket) {
-        handleUpdateBoard({ index, turn, winner, room });
-      } else if (room.player2 === socket) {
-        handleUpdateBoard({ index, turn, winner, room });
+      if (room.player1.socket === socket) {
+        newHandleUpdateBoard({
+          roomName,
+          index,
+          turn,
+          winner,
+          room,
+        });
+      } else if (room.player2.socket === socket) {
+        newHandleUpdateBoard({
+          roomName,
+          index,
+          turn,
+          winner,
+          room,
+        });
       }
     }
   });
 
   socket.on("resetGame", (roomName) => {
     const room = rooms.get(roomName);
-
     if (room) {
-      turnPlayer1 = true;
-      turnPlayer2 = false;
-      board = Array(9).fill(null);
+      room.player1 = {
+        socket,
+        activeTurn: true,
+        turn: "🦖",
+        playerType: "player1",
+      };
+      room.player2 = {
+        socket,
+        activeTurn: false,
+        turn: "🎸",
+        playerType: "player2",
+      };
+      (room.board = Array(9).fill(null)), rooms.set(roomName, room);
       room.player1.emit("changingTurns", {
-        turnState: turnPlayer1,
-        board: board,
+        turnState: room.player1.activeTurn,
+        board: room.board,
       });
-
       room.player2.emit("changingTurns", {
-        turnState: turnPlayer2,
-        board: board,
+        turnState: room.player2.activeTurn,
+        board: room.board,
       });
     }
   });
